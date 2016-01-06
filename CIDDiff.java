774399def5aadd170990a53c95969379295d3a74
Alek Bollig <a.bollig.school@gmail.com>
2016-01-05 20:47:02 -0500

    Added locale support.

--- CIDDiff.java
+++ CIDDiff.java
@@ -1,11 +1,10 @@
 /*/////////////////////////////////
- * CIDDiff V0.2
+ * CIDDiff V1.0
  * Creates readable difference reports for ClienItemDefinitions files from PS2.
  * Created by: Alek "binarycoder" Bollig, 2016
 *//////////////////////////////////
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -18,9 +17,12 @@ public class CIDDiff {
 		System.out.println("Finished.");
 		System.out.println("Scanning ClientItemDefinitions_new.txt");
 		ArrayList<ArrayList<String>> newData = parseString("ClientItemDefinitions_new.txt");
-		System.out.println("Finished");
+		System.out.println("Finished..");
+		System.out.println("Scanning en_us_data.dat");
+		ArrayList<String[]> nameData = parseLocale("en_us_data.dat");
+		System.out.println("Finished...");
 		System.out.println("Reading Differences");
-		ArrayList<String> resultantData = compareData(oldData, newData);
+		ArrayList<String> resultantData = compareData(oldData, newData, nameData);
 		System.out.println("Differences Found: ");
 		System.out.println("---------------------------------------------------------");
 		for (int i = 0; i < resultantData.size(); i++) {
@@ -28,6 +30,29 @@ public class CIDDiff {
 		}
 		System.out.println("---------------------------------------------------------");
 		
+	}
+	private static ArrayList<String[]> parseLocale(String fileLocation) {
+		File file = new File(fileLocation);
+		ArrayList<String[]> data = new ArrayList<String[]>();
+		try {
+			Scanner in = new Scanner(file);
+			while (in.hasNextLine()) {
+				String input = in.nextLine();
+				if (input.indexOf("u") != -1) {
+					String index = input.substring(0,(input.indexOf("u"))).trim();
+				String text = input.substring((input.indexOf("u")+4)).trim();
+				String[] addToArray = {index,text};
+				data.add(addToArray);
+				}		
+			}
+			in.close();
+			return data;
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: " + fileLocation
+					+ " not found in local directory. Exiting.");
+			e.printStackTrace();
+		}
+		return null;
 	}
 	static ArrayList<ArrayList<String>> parseString (String fileLocation) {
 		File file = new File(fileLocation);
@@ -49,9 +74,9 @@ public class CIDDiff {
 				}
 				dataPosition++;
 			}
+			fileRead.close();
 			return outputData;
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			System.out.println("Error: " + fileLocation
 					+ " not found in local directory. Exiting.");
 			e.printStackTrace();
@@ -59,14 +84,29 @@ public class CIDDiff {
 		return null;
 		
 	}
-	static ArrayList<String> compareData (ArrayList<ArrayList<String>> oldData, ArrayList<ArrayList<String>> newData) {
+	static ArrayList<String> compareData (ArrayList<ArrayList<String>> oldData,
+			ArrayList<ArrayList<String>> newData, ArrayList<String[]> nameData) {
 		ArrayList<String> output = new ArrayList<String>();
-		
 		while (newData.size() > oldData.size()) {
 			String outputString = "New Entry: ";
 			for (int i =0; i < newData.get(newData.size() - 1).size(); i++) {
-				outputString += "\n * " + newData.get(0).get(i) + ": "
+				if (i == 0) {
+					String realName = "Global.Text." + (newData.get(newData.size() - 1).get(2));
+					JenkinsHash jh = new JenkinsHash();
+					long realLong = jh.hash(realName.getBytes());
+					for (int j = 0; j < nameData.size(); j++) {
+						String[] tempArray = nameData.get(j);
+						if (tempArray[0].equals(String.valueOf(realLong))) {
+							realName = tempArray[1];
+						}
+					}
+					outputString += "\n * " + newData.get(0).get(i) + ": "
+							+ newData.get(newData.size() - 1).get(i)
+							+ " \"" + realName + "\"";
+				} else {
+					outputString += "\n * " + newData.get(0).get(i) + ": "
 						+ newData.get(newData.size() - 1).get(i);
+				}
 			}
 			output.add(outputString);
 			newData.remove(newData.size() - 1);
@@ -74,7 +114,16 @@ public class CIDDiff {
 		for (int i = 0; i < newData.size(); i ++) {
 			for (int j = 0; j < newData.get(i).size(); j++) {
 				if (!(oldData.get(i).get(j).equals(newData.get(i).get(j)))) {
-					output.add(newData.get(i).get(0) + "- " + newData.get(0).get(j) + " changed from "
+					String realName = "Global.Text." + (newData.get(i).get(2));
+					JenkinsHash jh = new JenkinsHash();
+					long realLong = jh.hash(realName.getBytes());
+					for (int k = 0; k < nameData.size(); k++) {
+						String[] tempArray = nameData.get(k);
+						if (tempArray[0].equals(String.valueOf(realLong))) {
+							realName = tempArray[1];
+						}
+					}
+					output.add(" \"" + realName + "\" " + "(" + newData.get(i).get(0) + ")- " + newData.get(0).get(j) + " changed from "
 							+ oldData.get(i).get(j) + " to " + newData.get(i).get(j));
 				}
 			}
